ca489d104274b6e4c393ff133219cd58fad313ef
szy042 <tlxxszy@126.com>
2019-12-12 00:35:14 +0800

    Add transform

--- src/renderer/core/transform.cpp
+++ src/renderer/core/transform.cpp
@@ -1,19 +1,146 @@
 #include "transform.h"
 
-Transform::Transform(Float m[16])
+Matrix4x4::Matrix4x4(const Float m[16])
 {
+	for (int i = 0; i < 16; i++){
+		m_m[i] = m[i];
+	}
 }
 
-void Transform::Identity()
+void Matrix4x4::Identity()
 {
+	for (int i = 0; i < 16; i++) {
+		m_m[i] = 0;
+	}
+	for (int i = 0; i < 4; i++){
+		m_m[i * 4 + i] = 1;
+	}
+}
+
+Matrix4x4 Matrix4x4::operator*(const Matrix4x4& t) const
+{
+	Float m[16];
+	for (int i = 0; i < 4; i++)	{
+		for (int j = 0; j < 4; j++)	{
+			m[i * 4 + j] = 0;
+			for (int k = 0; k < 4; k++)	{
+				m[i * 4 + j]+=this->m_m[i*4+k]*t.m_m[k*4+j];	
+			}
+		}
+	}
+	return Matrix4x4(m);
+}
+
+Matrix4x4& Matrix4x4::operator*=(const Matrix4x4& t)
+{
+	Float m[16];
+	for (int i = 0; i < 4; i++)	{
+		for (int j = 0; j < 4; j++)	{
+			m[i * 4 + j] = 0;
+			for (int k = 0; k < 4; k++)	{
+				m[i * 4 + j] += this->m_m[i * 4 + k] * t.m_m[k * 4 + j];
+			}
+		}
+	}
+	for (int i = 0; i < 16; i++){
+		this->m_m[i] = m[i];
+	}
+	return *this;
+}
+
+Matrix4x4 Inverse(const Matrix4x4& t)
+{
+	Float m[4][4];
+	Float mInv[4][4];
+	for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 4; j++)	{
+			m[i][j] = t.m_m[i * 4 + j];
+		}
+	}
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			mInv[i][j] = 0;
+		}
+	}
+	for (int i = 0; i < 4; i++) {
+		mInv[i][i] = 1;
+	}
+	int singularFlag = 0;
+	for (int i = 0; i < 3; i++){
+		if (m[i][i] == 0)	{
+			singularFlag = 1;
+			for (int j = i + 1; j < 4; j++)	{
+				if (m[j][i] != 0){
+					for (int k = i; k < 4; k++)	{
+						m[i][k] += m[j][k];
+					}
+					singularFlag = 0;
+					break;
+				}
+			}
+			ASSERT(singularFlag == 0, "Martrix is singular!");
+		}
+		for (int j = i+1; j < 4; j++){
+			Float ratio =- m[j][i]/m[i][i];
+			for (int k = i; k < 4; k++) {
+				m[j][k] += m[i][k] * ratio;
+			}
+			for (int k = 0; k < 4; k++) {
+				mInv[j][k] += mInv[i][k] * ratio;
+			}
+		}
+	}
+	ASSERT(m[3][3] == 0, "Martrix is singular!");
+	for (int i = 3; i > 0; i--) {
+		Float ratio = 1 / m[i][i];
+		for (int k = i; k < 4; k++){
+			m[i][k] *= ratio;
+		}
+		for (int k = 0; k < 4; k++) {
+			mInv[i][k] *= ratio;
+		}
+		for (int j = i - 1; j >= 0; j--) {
+			ratio = -m[j][i];
+			for (int k = i; k < 4; k++) {
+				m[j][k] += m[i][k] * ratio;
+			}
+			for (int k = 0; k < 4; k++) {
+				mInv[j][k] += mInv[i][k] * ratio;
+			}
+		}
+	}
+	return Matrix4x4((Float*)mInv);
+}
+
+
+
+Transform::Transform(const Float m[16]) :m_mat(m), m_invMat(Inverse(m_mat)) {}
+
+Transform::Transform(const Matrix4x4 m) :m_mat(m), m_invMat(Inverse(m_mat)) {}
+
+Transform::Transform(const Float m[16], const Float mInv[16]) :m_mat(m), m_invMat(mInv) {}
+
+Transform::Transform(const Matrix4x4 m, const Matrix4x4 mInv) :m_mat(m), m_invMat(mInv) {}
+
+void Transform::Identity() 
+{
+	m_mat.Identity();
+	m_invMat.Identity();
+}
+
+Transform Inverse(const Transform& t)
+{
+	return Transform(t.m_invMat,t.m_mat);
 }
 
 Transform Transform::operator*(const Transform& t) const
 {
-    return Transform();
+	return Transform(m_mat*t.m_mat, t.m_invMat*m_invMat);
 }
 
 Transform& Transform::operator*=(const Transform& t)
 {
-    // TODO: insert return statement here
+	m_mat *= t.m_mat;
+	m_invMat = t.m_invMat * m_invMat;
+	return *this;
 }
--- src/renderer/core/transform.h
+++ src/renderer/core/transform.h
@@ -8,26 +8,36 @@ class Matrix4x4 {
 public:
     Matrix4x4() {}
     Matrix4x4(const Float m[16]);
-
+  
     void Identity();
 
     Matrix4x4 operator * (const Matrix4x4& t) const;
     Matrix4x4& operator *= (const Matrix4x4& t);
 
-    Float m[16];
+    Float m_m[16];
+
+    friend Matrix4x4 Inverse(const Matrix4x4& t);
 };
 
 class Transform {
 public:
     Transform() {}
     Transform(const Float m[16]);
+    Transform(const Matrix4x4 m);
+    Transform(const Float m[16],const Float mInv[16]);
+    Transform(const Matrix4x4 m, const Matrix4x4 mInv);
+    Transform(const Matrix4x4 m, const Float mInv[16]);
+    Transform(const Float m[16], const Matrix4x4 mInv);
 
     void Identity();
 
     Transform operator * (const Transform& t) const;
     Transform& operator *= (const Transform& t);
+    
 
-    Matrix4x4 mat, invMat;
+    Matrix4x4 m_mat, m_invMat;
+
+    friend Transform Inverse(const Transform& t);
 };
 
 #endif // __TRANSFORM_H
